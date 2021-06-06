f65f4b8621bc7964c8032cc2587af8f6298c035d
ARAPHELLE <ARAPHELLE@users.noreply.github.com>
2021-06-06 14:32:24 -0400

    Updated UI elements and added functionality to all menu buttons except "About"

---
+++ Soil/Assets/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadTestWorld()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenGitHub()
+    {
+        Application.OpenURL("https://github.com/ARAPHELLE/Soil");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
