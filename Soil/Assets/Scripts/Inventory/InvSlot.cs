3751e34ea210d97235a9773a997a56437cc3f64c
wynaut-wastaken <wynaut.wastaken@gmail.com>
2021-06-06 15:29:18 -0400

    Imported Mirror networking, made a few inventory-related scripts

---
+++ Soil/Assets/Scripts/Inventory/InvSlot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvSlot 
+{
+    public static Item empty = new Item();
+
+    public Item contained;
+}
---
+++ Soil/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Item
+{
+    public enum Rarity { Crap, Common, Uncommon, Rare, Amazing, Legendary}
+
+    public Rarity rarity;
+
+    public GameObject dropPrefab;
+
+    public Texture2D inventorySprite;
+
+    public string displayName;
+
+    public string description;
+}
